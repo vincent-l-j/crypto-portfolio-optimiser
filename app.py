e9794bfa7980d139d8b3c21f9cb3f1142c03cb15
Vincent Jadraque <vincent.jadraque@gmail.com>
2022-01-11 12:24:13 +1100

    Add template pages to streamlit dashboard

--- app.py
+++ app.py
@@ -5,11 +5,17 @@ and his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page
 from src.multiapp import MultiApp
 from src import (
     home,
+    historical_performance,
+    portfolio_forecasting,
+    summary,
 )
 
 app = MultiApp()
 
 # Add all your apps here
 app.add_app("Home", home.app)
+app.add_app("Historical Performance", historical_performance.app)
+app.add_app("Portfolio Forecasting", portfolio_forecasting.app)
+app.add_app("Summary", summary.app)
 # The main app
 app.run()
