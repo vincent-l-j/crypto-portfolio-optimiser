0058be5ba697df051ffe6e90ad8a047cd6d63fcb
vincent-l-j <vincent.jadraque@gmail.com>
2022-01-13 21:08:09 +1100

    updated ylabels

--- src/historical_performance.py
+++ src/historical_performance.py
@@ -11,8 +11,8 @@ def app():
         st.write('Please build your portfolio first.')
         return
     df_daily_returns = st.session_state.df_daily_returns
-    df_daily_returns_plt = df_daily_returns.hvplot(figsize=(25,10), xlabel="Year", ylabel="Percentage change", title="Daily Returns over 1 Year")
+    df_daily_returns_plt = df_daily_returns.hvplot(figsize=(25,10), xlabel="Year", ylabel="Daily Returns", title="Daily Returns over 1 Year")
     cumulative_returns = (1 + df_daily_returns).cumprod() - 1
-    cum_returns_plt = cumulative_returns.hvplot(figsize=(25,10), xlabel="Year", ylabel="Percentage change", title="Cumulative Returns over 1 Year")
+    cum_returns_plt = cumulative_returns.hvplot(figsize=(25,10), xlabel="Year", ylabel="Cumulative Returns", title="Cumulative Returns over 1 Year")
     st.bokeh_chart(hv.render(df_daily_returns_plt, backend='bokeh'))
     st.bokeh_chart(hv.render(cum_returns_plt, backend='bokeh'))
