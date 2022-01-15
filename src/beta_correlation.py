5cdf65bc0a5042549dd5259f5652597cfb6476b2
Vincent Jadraque <vincent.jadraque@gmail.com>
2022-01-15 20:27:48 +1100

    WIP Fix dataframe in beta correlation

--- src/beta_correlation.py
+++ src/beta_correlation.py
@@ -7,22 +7,26 @@ import numpy as np
 def app():
     st.title('Portfolio Statistics')
 
-    df_change = st.session_state.df_daily_returns
+    asset_list = st.session_state.weightings.keys()
+    df_ohlcv = st.session_state.df_ohlcv
+    df_close = df_ohlcv[pd.MultiIndex.from_product([asset_list + ['BTC'], ['close']])].droplevel(1, axis=1)
+    df_change_and_BTC = df_close.pct_change().dropna()
+    df_change = df_change_and_BTC[asset_list]
 
     v = list(st.session_state.weightings.values())
     weights = v/np.linalg.norm(v)
     portfolio_return = weights * df_change
     portfolio_sum = portfolio_return.sum(axis=1)
 
-    variance_btc = df_change['BTC'].var()
-    covariance_portfolio = portfolio_sum.cov(df_change['BTC'])
+    variance_btc = df_change_and_BTC['BTC'].var()
+    covariance_portfolio = portfolio_sum.cov(df_change_and_BTC['BTC'])
     portfolio_beta = covariance_portfolio / variance_btc
 
-    rolling_variance = df_change['BTC'].rolling(window=30).var()
-    rolling_covariance = portfolio_sum.rolling(window=30).cov(df_change['BTC'])
+    rolling_variance = df_change_and_BTC['BTC'].rolling(window=30).var()
+    rolling_covariance = portfolio_sum.rolling(window=30).cov(df_change_and_BTC['BTC'])
     rolling_beta = rolling_covariance / rolling_variance 
 
-    combined_df = pd.concat([df_change['BTC'], portfolio_sum], axis='columns', join='inner')
+    combined_df = pd.concat([df_change_and_BTC['BTC'], portfolio_sum], axis='columns', join='inner')
     combined_df.rename(columns={
     combined_df.columns[1]: 'Portfolio', 
     combined_df.columns[0]: 'BTC'
